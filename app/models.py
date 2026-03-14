d45363ec8bc69bba19e7d69b1c7e4a703baefff8
Olufunke Ogunde <olufunkeogunde@outlook.com>
2026-03-14 17:26:18 -0500

    Exercise 3 Completed

--- app/models.py
+++ app/models.py
@@ -19,7 +19,8 @@ class UserProfile(db.Model):
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
-        self.password = generate_password_hash(password)
+        self.password = generate_password_hash(password, method="pbkdf2:sha256:600000")
+        len(self.password)
 
     def is_authenticated(self):
         return True
